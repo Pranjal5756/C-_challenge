8fab218f05c2aefaaca6c080b5c3cf62c916bf1d
Pranjal Pratik <pratikpranjal72@gmail.com>
2024-01-17 02:16:56 +0530

    practiced loop

--- loops/forloop/forloop.cpp
+++ loops/forloop/forloop.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout << "do while loo" << "\n";
-    cout << "okay";
+// int main(){
+//     for(int i = 1; i<=10; i++){
+
+//         // cout << "2" <<" * "<< i << " = " << " " << mul << "\n";
+//         for(int j = 1; j<=10; j++){
+//             int mul = j * i;
+//             cout << i << " " << " * " << j << " " << " = " << mul << "\n";
+//             // if()
+//         }
+//     }
+//     return 0;
+// }
+
+int main()
+{
+
+    for (int i = 9; i <= 10; i++)
+    {
+        // cout << i << "\n
+
+        for (int j = 1; j <= 10; j++)
+        {
+            // cout << j << "\t" << i << "\n";
+            cout << i << "\n";
+        }
+    }
+    
     return 0;
 }
