6496257a7a2e2befdc35ce6939210e8a61305fc5
Pranjal Pratik <pratikpranjal72@gmail.com>
2024-02-13 04:10:48 +0530

    #

--- hello.world/Hello.cpp
+++ hello.world/Hello.cpp
@@ -1,8 +1,14 @@
 // make script 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-  cout << "Hello World!";
+  bool mybo = true;
+  double mydo = 948.88;
+  float myflo = 948.88;
+  char mychar = 'h';
+  string MyName = "pritam";
+  cout << MyName << "\n" << mychar << "\n" << mydo << "\n" << myflo << "\n" << mybo ;
   return 0;
 }
---
+++ pritam/question/addition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+    int prit1_am1 = 43;
+
+    prit1_am1 = 59;
+
+    cout << prit1_am1;
+    
+    
+    return 0;
+}
